8b989727aa49df84b8cb5474e200a617d7b413b4
jgen0005 <jgen0005@student.monash.edu>
2023-06-05 08:15:51 +1000

    signin ui config

---
+++ src/components/UploadImage.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+import { Auth } from 'aws-amplify';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const UploadImage = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const checkUser = async () => {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      console.log('User: ', user);
+    } catch (error) {
+      console.log('Error: ', error);
+      // Redirect to home if not logged in
+      Auth.federatedSignIn();
+    }
+  };
+
+  const handleImageChange = (e) => {
+    setSelectedImage(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    setUploading(true);
+
+    const reader = new FileReader();
+    reader.onloadend = async () => {
+      const base64Data = reader.result.split(',')[1];
+
+      const currentUser = await Auth.currentAuthenticatedUser();
+      const token = currentUser.signInUserSession.idToken.jwtToken;
+
+      const config = {
+        headers: {
+          Authorization: token,
+        },
+      };
+
+      // Replace 'https://your-api-gateway-url' with your actual API Gateway URL
+      const url = 'https://ivaylef3bi.execute-api.us-east-1.amazonaws.com/dev/upload';
+
+      const body = {
+        body: base64Data,
+        uuid: currentUser.attributes.email,
+        path: selectedImage.name   // adding the filename to the request body
+      };
+
+      try {
+        const response = await axios.post(url, body, config);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error uploading image: ', error);
+      }
+
+      setUploading(false);
+    };
+
+    reader.readAsDataURL(selectedImage);
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+      <Button onClick={handleUpload} disabled={uploading}>
+        {uploading ? 'Uploading...' : 'Upload'}
+      </Button>
+    </div>
+  );
+};
+
+export default UploadImage;
