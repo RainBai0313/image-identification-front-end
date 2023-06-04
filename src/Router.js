e70c4fa7e4bff858428e4d4d9ffdeeaf24d09499
jgen0005 <jgen0005@student.monash.edu>
2023-06-05 03:16:29 +1000

    fix import router

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,18 @@
 // src/Router.js
 
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import UploadImage from './components/UploadImage';
 // import your other components...
 
 const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/upload" component={UploadImage} />
-      {/* Add your other routes here */}
-    </Switch>
-  </BrowserRouter>
+    <Router>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+    </Router>
 );
 
 export default Router;
