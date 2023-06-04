51c20518c63a16cc1429b85f529c39bfcbed9fa0
jgen0005 <jgen0005@student.monash.edu>
2023-06-05 03:08:36 +1000

    fix import amplify

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Amplify from 'aws-amplify';
-import config from './AmplifyConfig'; // Adjust this path to point to your AmplifyConfig.js file
+import * as Amplify from 'aws-amplify';
+import config from './config/AmplifyConfig';
 
 Amplify.configure(config);
 
