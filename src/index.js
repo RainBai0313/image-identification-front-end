d61239ef729aa6c933b82e6dee4dc6c97c60de2b
jgen0005 <jgen0005@student.monash.edu>
2023-06-05 03:26:48 +1000

    fix amplify

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as Amplify from 'aws-amplify';
-import config from './config/AmplifyConfig';
+import { Amplify } from 'aws-amplify';
 
-Amplify.configure(config);
+Amplify.configure({
+  Auth: {
+      region: 'us-east-1',
+      userPoolId: 'us-east-1_9tVQ8XxK6',
+      userPoolWebClientId: '7e8ho0ofisu83pbmcm4ivsudot',
+      authenticationFlowType: 'USER_PASSWORD_AUTH',
+  },
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
